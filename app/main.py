82890d82560bc817d42171ea7fbc5cf0f3840bf5
Nagendra M <nagendram@propelapps.com>
2025-10-05 17:06:39 +0530

    wy1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 import socket  # noqa: F401
 
+def handle_connection(connection):
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            connection.sendall(b"+PONG\r\n")
+    except ConnectionResetError:
+        print("Connection was reset by the client.")
+    finally:
+        connection.close()
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,10 +19,17 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
-
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379))
+    try:
+        while True:
+            connection, _ = server_socket.accept() # wait for client
+            handle_connection(connection)
+    except KeyboardInterrupt:
+        print("Shutting down server.")
+    finally:
+        server_socket.close()
+        
 
 if __name__ == "__main__":
     main()
